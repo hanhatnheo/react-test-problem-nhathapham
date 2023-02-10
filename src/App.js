93e8bd99d2303abaf8fe889c6b1d9636e9c0c273
hanhatnheo <phamjudy2004@gmail.com>
2023-02-10 03:05:50 -0500

    Submission

--- src/App.js
+++ src/App.js
@@ -4,17 +4,38 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      msg: "click me"
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  //handler goes here
+  handleClick = (msg) => {
+    if(this.state.msg === "click me") {
+      this.setState({msg: "thanks"});
+    } else {
+      this.setState({msg: "click me"});
+    }
+    
   }
 
-  //handler goes here
-
   render() {
+    const { msg } = this.state;
+    let body = document.body;
+    body.style.background = '#23395d';
     return (
-      <div>
-        <p>Greetings!</p>
-        <p>Button goes here</p>
+      <div style={{textAlign: "center"}}>
+        <p style={{fontSize: '2rem'}}>
+        <font color="white">
+        <span style={{fontWeight: 'bold'}}>
+          Greetings!
+        </span>
+        </font>  
+        </p>
+        <button style={{fontFamily:'Roboto', width: "100px", height: "30px", fontSize: 16}} 
+                onClick={this.handleClick}>
+          {msg}
+        </button>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,29 @@
+import { composeStories } from '@storybook/testing-react';
 import { render, screen, fireEvent } from '@testing-library/react';
 
 // import your story from App.stories.js
-
+import * as stories from './App.stories';
+const { Primary } = composeStories(stories);
 
 // Add your tests here
+it('Button text initially says click me', async () => {
+    render(<Primary />);
+    expect(screen.getByText(/click me/i)).not.toBeNull()
+});
+
+it('Button can change to thanks', async () => {
+    render(<Primary />);
+    screen.getByRole('button').click();
+    const clickedButton = screen.getByRole('button', { name : 'thanks'});
+    expect(clickedButton).toBeInTheDocument();
+});
+
+it('Clicking button changes to thanks', async () => {
+    render(<Primary />);
+    const button = screen.getByRole('button', { name : 'click me'});
+    fireEvent.click(button);
+    const clickedButton = screen.getByRole('button', { name : 'thanks'});
+    expect(clickedButton).toBeInTheDocument();
+});
+
 // See https://storybook.js.org/docs/react/writing-tests/importing-stories-in-tests#example-with-testing-library
